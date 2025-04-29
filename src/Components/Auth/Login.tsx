6d25390914407a3f27f70ed8e6a7e0f47d518672
KaranSapra124 <sapra7112@gmail.com>
2025-04-29 23:19:40 +0530

    Almost finished login

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -3,21 +3,18 @@ import { Link } from "react-router-dom"
 const Login = () => {
     return (
         <>
-            <div className="flex max-w-screen-md mx-auto ">
+            <div className="flex shadow max-w-screen-md mx-auto ">
                 <div className="bg-primary w-1/2 p-5">
                     <h1 className="text-white font-bold lg:text-3xl text-2xl">Welcome My Friend!</h1>
                     <p className="text-white lg:text-sm text-xs font-semibold"> Please log in to WorkSphere and experience seamless HR management at your fingertips.</p>
                     <button className="bg-white text-black py-1 px-4 rounded-lg my-2 ">Sign Up</button>
                 </div>
-                <div className="flex flex-col">
-                    <h1>Create An Account</h1>
-                    <div>
-
-                    </div>
-                    <div className="flex flex-col">
-                        <input placeholder="Enter Your Email..." type="email" name="" id="" />
-                        <input placeholder="Enter Your Email..." type="password" name="" id="" />
-                        <button>Log in</button>
+                <div className="flex justify-center items-center mx-auto flex-col gap-5 p-6">
+                    <h1 className="text-primary text-center font-bold lg:text-2xl text-xl">Create An Account</h1>
+                    <div className="flex gap-5 flex-col">
+                        <input className="p-2 shadow text-primary font-semibold" placeholder="Enter Your Email..." type="email" name="" id="" />
+                        <input className="p-2 shadow text-primary font-semibold" placeholder="Enter Your Password..." type="password" name="" id="" />
+                        <button className="bg-primary text-white py-2 px-4 rounded-lg">Log in</button>
                     </div>
                 </div>
             </div>
