857fbd5b8731f7ae2121efbec7dae32c5b8008b9
KaranSapra124 <sapra7112@gmail.com>
2025-04-29 23:14:43 +0530

    started styling the login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './Components/Auth/Login'
 
 function App() {
 
   return (
     <>
-     
+     <Login/>
     </>
   )
 }
--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom"
 const Login = () => {
     return (
         <>
-            <div>
-                <div className="bg-primary">
-                    <h1 className="text-white font-bold">Welcome My Friend!</h1>
-                    <p className="text-white"> Please log in to WorkSphere and experience seamless HR management at your fingertips.</p>
-                    <Link to={"/"}>Sign Up</Link>
+            <div className="flex max-w-screen-md mx-auto ">
+                <div className="bg-primary w-1/2 p-5">
+                    <h1 className="text-white font-bold lg:text-3xl text-2xl">Welcome My Friend!</h1>
+                    <p className="text-white lg:text-sm text-xs font-semibold"> Please log in to WorkSphere and experience seamless HR management at your fingertips.</p>
+                    <button className="bg-white text-black py-1 px-4 rounded-lg my-2 ">Sign Up</button>
                 </div>
-                <div>
+                <div className="flex flex-col">
                     <h1>Create An Account</h1>
                     <div>
 
                     </div>
-                    <div>
-                        <input type="email" name="" id="" />
-                        <input type="password" name="" id="" />
+                    <div className="flex flex-col">
+                        <input placeholder="Enter Your Email..." type="email" name="" id="" />
+                        <input placeholder="Enter Your Email..." type="password" name="" id="" />
                         <button>Log in</button>
                     </div>
                 </div>
