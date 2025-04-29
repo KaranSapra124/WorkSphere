5b1a68683b91f86d40d51a5d64307ea45c46b1e7
KaranSapra124 <sapra7112@gmail.com>
2025-04-29 23:04:50 +0530

    Started Login Page

---
+++ src/Components/Auth/Login.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const Login = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>Welcome My Friend!</h1>
+                    <p> Please log in to WorkSphere and experience seamless HR management at your fingertips.</p>
+                    <Link to={"/"}>Sign Up</Link>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
