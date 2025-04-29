c36e2b9ee73c32effb7e2cc5a8e94c7df34c2503
KaranSapra124 <sapra7112@gmail.com>
2025-04-29 23:41:59 +0530

    Almost finished login

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
             <div className="flex justify-center items-center h-screen">
                 <div className="flex  shadow h-fit  max-w-screen-md mx-auto ">
                     <div className="bg-primary flex flex-col justify-center items-center gap-3  w-1/2 p-5">
-                        <h1 className="text-white font-bold lg:text-3xl text-2xl">Welcome My Friend!</h1>
+                        <h1 className="text-white font-extrabold lg:text-2xl text-xl">Welcome My Friend!</h1>
                         <p className="text-white lg:text-md text-xs font-semibold text-center"> Please log in to <span className="font-extrabold">WorkSphere</span> and experience seamless HR management at your fingertips.</p>
                         <button className=" text-white border border-white  py-1 px-8 lg:text-lg cursor-pointer hover:scale-105 transition-all text-sm rounded-l-full rounded-r-full my-2 ">Sign Up</button>
                     </div>
