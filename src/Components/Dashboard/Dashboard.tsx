4b46d72967a061dc98943d2e507243a465a3112f
KaranSapra124 <sapra7112@gmail.com>
2025-05-01 01:10:42 +0530

    Added Items in dashboard

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Dashboard = () => {
     return (
         <>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
+                <div className="drawer-content flex flex-col items-center justify-self-start">
                     {/* Page content here */}
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
                         Open drawer
@@ -15,14 +13,21 @@ const Dashboard = () => {
                     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                         {/* Sidebar content here */}
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                        <li><a>Dashboard</a></li>
+                        <li><a>Reports</a></li>
+
+                        <li className="menu-title">
+                            <span>Essentials</span>
+                        </li>
+                        <li><a>Employee Directory</a></li>
+                        <li><a>Leave Tracker</a></li>
+                        <li><a>Attendance</a></li>
+                        <li><a>Payroll</a></li>
                     </ul>
                 </div>
             </div>
-
         </>
-    )
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
