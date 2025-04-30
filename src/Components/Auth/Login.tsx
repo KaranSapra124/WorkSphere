3e1aee94b860b011148269d992d348f65de05478
KaranSapra124 <sapra7112@gmail.com>
2025-05-01 00:13:50 +0530

    Added login and signup

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -26,7 +26,7 @@ const Login: React.FC<{ isLogin: isLogin }> = ({ isLogin }) => {
                     <div className="bg-primary flex flex-col justify-center items-center gap-3  w-1/2 p-5">
                         <h1 className="text-white font-extrabold lg:text-2xl text-xl">Welcome My Friend!</h1>
                         <p className="text-white lg:text-md text-xs font-semibold text-center"> Please log in to <span className="font-extrabold">WorkSphere</span> and experience seamless HR management at your fingertips.</p>
-                        <Link to={"/sign-up"}  className=" text-white border border-white  py-1 px-8 lg:text-lg cursor-pointer hover:scale-105 transition-all text-sm rounded-l-full rounded-r-full my-2 hover:bg-white hover:text-primary ">Sign Up</Link>
+                        <Link to={"/login"} className=" text-white border border-white  py-1 px-8 lg:text-lg cursor-pointer hover:scale-105 transition-all text-sm rounded-l-full rounded-r-full my-2 hover:bg-white hover:text-primary ">Login</Link>
                     </div>
                     <div className="flex justify-center items-center mx-auto flex-col gap-5 p-6">
                         <h1 className="text-primary text-center font-bold lg:text-2xl text-xl">Create An Account</h1>
